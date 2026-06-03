4164f59749c3e0db4a69e671efa1a94dbe04d0c5
Spudsly <Robert.c.brewster@gmail.com>
2026-06-03 10:44:19 -0700

    all files: remove stick from main loop - only fire once on /engage

--- classLUA/mnk.lua
+++ classLUA/mnk.lua
@@ -281,9 +281,6 @@ while true do
 
     if engaged and targetValid() then
         mq.cmd('/attack on')
-        if tonumber(mq.TLO.Target.Distance()) > 20 then
-            mq.cmd('/stick 15 uw behind loose hold')
-        end
         doAbilities()
     end
 
